4e167b34b5039d193e7caee93c68d9309008237f
kimdayeon37 <kdy37912@naver.com>
2023-08-25 17:42:49 +0900

    skeleton UI loading

--- dist/index.4d6bcbeb.js
+++ dist/index.4d6bcbeb.js
@@ -789,7 +789,9 @@ class Search extends (0, _heropy.Component) {
     render() {
         this.el.classList.add("search");
         this.el.innerHTML = /* html */ `
-        <input placeholder="Enter the movie title to search!" />
+        <input 
+        value="${(0, _movieDefault.default).state.searchText}" 
+        placeholder="Enter the movie title to search!" />
         <button class="btn btn-primary">
             Search
     </button>
@@ -965,12 +967,22 @@ var _movie = require("../store/movie");
 var _movieDefault = parcelHelpers.interopDefault(_movie);
 class Movie extends (0, _heropy.Component) {
     async render() {
+        this.el.classList.add("container", "the-movie");
+        this.el.innerHTML = /* html */ `
+        <div class="poster skeleton"></div>
+        <div class="specs">
+            <div class="title skeleton"></div>
+            <div class="label skeleton"></div>
+            <div class="plot skeleton"></div>
+        </div>
+        `;
         await (0, _movie.getMovieDetails)(history.state.id);
         console.log((0, _movieDefault.default).state.movie);
         const { movie } = (0, _movieDefault.default).state;
-        this.el.classList.add("container", "the-movie");
+        // 고해상도 이미지
+        const bigPoster = movie.Poster.replace("SX300", "SX700");
         this.el.innerHTML = /* html */ `
-        <div style="background-image: url(${movie.Poster})" class="poster"></div>
+        <div style="background-image: url(${bigPoster})" class="poster"></div>
         <div class="specs">
             <div class="title">
                 ${movie.Title}
