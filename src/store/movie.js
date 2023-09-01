f8e9e3011d526457246f92f84c663156b30a1114
kimdayeon37 <kdy37912@naver.com>
2023-09-01 10:49:25 +0900

    코드 수정

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,61 +1,62 @@
 import { Store } from '../core/heropy'
 
 const store = new Store({
-    searchText: '',
-    page: 1,
-    pageMax: 1,
-    movies: [],
-    movie: {},
-    loading: false,
-    message: 'Search for the movie title!'
+  searchText: '',
+  page: 1,
+  pageMax: 1,
+  movies: [],
+  movie: {},
+  loading: false,
+  message: 'Search for the movie title!'
 })
 
 export default store
 export const searchMovies = async page => {
-    store.state.loading = true
-    store.state.page = page
-    if (page === 1){
-        store.state.movies = []
-        store.state.message = ''
+  store.state.loading = true
+  store.state.page = page
+  if (page === 1) {
+    store.state.movies = []
+    store.state.message = ''
+  }
+  try {
+    // serverless와 통신
+    // const res = await fetch(`https://omdbapi.com?apikey=7035c60c&s=${store.state.searchText}&page=${page}`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      })
+    })
+    const { Response, Search, totalResults, Error } = await res.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies,
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      store.state.message = Error
+     // 추가 페이지 상태(pageMax) 값이 2 이상일때 영화목록을 가져올 수 없는 상황이되면 상태를 초기화
+      store.state.pageMax = 1
     }
-    try {
-        // serverless와 통신
-        const res = await fetch('/api/movie', {
-            method: "POST",
-            body: JSON.stringify({
-                title: store.state.searchText,
-                page
-            })
-        })
-        const {Search, totalResults, Response, Error } = await res.json()
-        if (Response === 'True') {
-            store.state.movies = [
-                ...store.state.movies,
-                ...Search
-            ]
-            store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-        } else {
-            store.state.message = Error
-            // 추가 페이지 상태(pageMax) 값이 2 이상일때 영화목록을 가져올 수 없는 상황이되면 상태를 초기화
-            store.state.pageMax = 1
-        }
-    } catch (error) {
-        console.log('searchMovies error:', error)
-    } finally {
-        store.state.loading = false// loading 종료
-    }
+  } catch (error) {
+    console.log('searchMovies error:', error)
+  } finally {
+    store.state.loading = false // loading 종료
+  }
 }
-
 export const getMovieDetails = async id => {
-    try {
-        const res = await fetch('/api/movie',{
-            method: "POST",
-            body: JSON.stringify({
-                id
-            })
-        })
-        store.state.movie = await res.json()
-    } catch(error) {
-        console.log('getMovieDetails error:', error)
-    }
+  try {
+    // const res = await fetch(`https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`)
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
+    store.state.movie = await res.json()
+  } catch (error) {
+    console.log('getMovieDetails error:', error)
+  }
 }
